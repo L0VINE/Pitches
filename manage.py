fdbf94b6894082607760425962cf1f922dee1ff1
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-08-22 18:26:15 +0300

    changed from flask-script to cli

--- manage.py
+++ manage.py
@@ -1,31 +1,13 @@
 from app import create_app
 from app import db
-from flask_script import Manager,Server
-from  flask_migrate import Migrate, MigrateCommand
+from  flask_migrate import Migrate
 from app.models import User, Pitches, Comments
 
 
 app = create_app('development')
 
-
-
-manager = Manager(app)
-manager.add_command('server',Server)
-
-
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-@manager.shell
+migrate  = Migrate(app,db)
+@app.shell_context_processor
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Pitches = Pitches, Comments= Comments)
+    return dict(db = db,User = User,Pitches =Pitches, Comments=Comments )
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
-
-if __name__ == '__main__':
-    manager.run()

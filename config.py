c9eb4afeb7bf8942b87d4274b512a52bb04d40b5
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-08-25 13:48:09 +0300

    Changed configurations

--- config.py
+++ config.py
@@ -28,7 +28,9 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+      SQLALCHEMY_DATABASE_URI= SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     
 
 class TestConfig(Config):
